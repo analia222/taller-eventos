16fd68cceb28904263263edaf07f252d98a56aa5
analia222 <analiamira23@gmail.com>
2025-08-09 22:01:49 -0300

    eventos

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
-function saludar() {
+//function saludar() {
     alert('Hola!');
-}
+//}
+
+
+const divContenedor = document.getElementById('contenedor-boton');
+
+divContenedor.addEventListener('click', function() {
+  alert('Hola! Soy el div');
+});
